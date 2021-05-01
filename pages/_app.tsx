ca11eeaa09b91f5b53ee0e77288c679f737a6da4
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-01 14:52:07 +0100

    lint and remove comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ import { lightTheme, darkTheme } from '../theme';
 import Head from 'next/head';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
-config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
 	const [isMounted, setIsMounted] = useState(false);
