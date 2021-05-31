8af8e389b0ad5716717568da7e42fa3559baa301
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-31 07:01:45 +0100

    more info on vext

--- src/pages/vext.tsx
+++ src/pages/vext.tsx
@@ -29,6 +29,9 @@ export default function Vext() {
 					<GitHubRepo repo="Milo123459/vext" />
 
                     <Code><i>Still a WIP!</i></Code>
+
+                    <Code>important note, i'm writing this as a side project.</Code>
+                    <Code>i hope to have it finished by the end of summer 2021</Code>
 				</div>
 			</main>
 		</div>
