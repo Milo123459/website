d841ffbbbfbf3de66b359ac2681a5259ceddabed
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-09 09:29:59 +0100

    get a map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -3,21 +3,15 @@ import Sidebar from '../components/Sidebar';
 import Code from '../components/Code';
 import CodeLink from '../components/CodeLink';
 
-export default function Home() {
+export default function Map() {
 	return (
 		<div className={styles.container}>
 			<Sidebar />
 			<main className={styles.main}>
 				<div style={{ paddingLeft: '3%' }}>
 					<Code>
-						you seem lost,{' '}
-						<CodeLink link="/map" colour="#48bad7" noPre={true}>
-							either get a map
-						</CodeLink>
-						, or{' '}
-						<CodeLink link="/" noPre={true} colour="pink">
-							go home
-						</CodeLink>
+						i'm a map
+						<Code>respect maps please</Code>
 					</Code>
 				</div>
 			</main>
