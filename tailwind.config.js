4e60ee83fd96c5b0a13e69642574a36c56de6421
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-03 17:42:18 +0100

    disable jit as unstable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
-	mode: 'jit',
+	darkMode: false,
 	theme: {
 		extend: {},
 	},
