fd009f916799b295c08483ef77e27888113bd2a6
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-03 17:09:34 +0100

    A fresh start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,3 @@
-import styles from '../styles/Base.module.css';
-import Sidebar from '../components/Sidebar';
-import Code from '../components/Code';
-
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<Sidebar />
-			<main className={styles.main}>
-				<div style={{ paddingLeft: '3%' }}>
-					<Code>hi</Code>
-					<Code>im milo</Code>
-					<Code>i love writing code</Code>
-					<Code>i do it too much</Code>
-					<Code>i seriously need help</Code>
-					<Code>thanks for visiting</Code>
-				</div>
-			</main>
-		</div>
-	);
+	return <div>A fresh start.</div>;
 }
