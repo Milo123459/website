4632ea6c0376c6cb694f75f4e27e7981ed6988e2
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-05 07:55:39 +0100

    Lint

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { Text } from '@geist-ui/react';
 export default function NotFound() {
 	return (
 		<Text h3 b>
-			This page can't be found. Maybe go home?
+			This page can&apos;t be found. Maybe go home?
 		</Text>
 	);
 }
