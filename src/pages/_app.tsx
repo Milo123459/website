da61c42907726597e40679d1a62d46dd88391d36
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-05 17:27:30 +0100

    Add toggle for dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,12 @@
-import { GeistProvider, CssBaseline, Page, Breadcrumbs } from '@geist-ui/react';
+import {
+	GeistProvider,
+	CssBaseline,
+	Page,
+	Breadcrumbs,
+	Select,
+	Container,
+	Spacer,
+} from '@geist-ui/react';
 import 'modern-normalize/modern-normalize.css';
 import 'inter-ui/inter.css';
 import React from 'react';
@@ -10,12 +18,8 @@ import { Home, TrendingUp, Inbox, Tv } from '@geist-ui/react-icons';
 
 function MyApp({ Component, pageProps }) {
 	const route = useRouter();
-	const [isMounted, setIsMounted] = React.useState(false);
 	const darkMode = useDarkMode(true);
-	const themeType = 'light';
-	React.useEffect(() => {
-		setIsMounted(true);
-	}, []);
+	const themeType = darkMode.value == true ? 'light' : 'dark';
 	return (
 		<>
 			<Head>
@@ -29,53 +33,62 @@ function MyApp({ Component, pageProps }) {
 			<GeistProvider themeType={themeType}>
 				<CssBaseline />
 				<Page dotBackdrop>
-					<Breadcrumbs size="medium">
-						<NextLink href="/">
-							<Breadcrumbs.Item
-								nextLink
-								aria-disabled={route.route == '/'}
-								style={{ color: route.route == '/' ? '#333' : '' }}
-							>
-								<Home />
-							</Breadcrumbs.Item>
-						</NextLink>
-						<NextLink href="/stats">
-							<Breadcrumbs.Item
-								nextLink
-								aria-disabled={route.route == '/stats'}
-								style={{ color: route.route == '/stats' ? '#333' : '' }}
-							>
-								<TrendingUp />
-							</Breadcrumbs.Item>
-						</NextLink>
-						<NextLink href="/notifications">
-							<Breadcrumbs.Item
-								nextLink
-								aria-disabled={route.route == '/notifications'}
-								style={{ color: route.route == '/notifications' ? '#333' : '' }}
-							>
-								<Inbox />
-							</Breadcrumbs.Item>
-						</NextLink>
-						<NextLink href="/tuber">
-							<Breadcrumbs.Item
-								nextLink
-								aria-disabled={route.route == '/tuber'}
-								style={{ color: route.route == '/tuber' ? '#333' : '' }}
-							>
-								<Tv />
-							</Breadcrumbs.Item>
-						</NextLink>
-						<NextLink href="/notes">
-							<Breadcrumbs.Item
-								nextLink
-								aria-disabled={route.route == '/notes'}
-								style={{ color: route.route == '/notes' ? '#333' : '' }}
-							>
-								NOTEPAD_ICON
-							</Breadcrumbs.Item>
-						</NextLink>
-					</Breadcrumbs>
+					<Container>
+						<Breadcrumbs size="medium">
+							<NextLink href="/">
+								<Breadcrumbs.Item
+									nextLink
+									aria-disabled={route.route == '/'}
+									style={{ color: route.route == '/' ? '#333' : '' }}
+								>
+									<Home />
+								</Breadcrumbs.Item>
+							</NextLink>
+							<NextLink href="/stats">
+								<Breadcrumbs.Item
+									nextLink
+									aria-disabled={route.route == '/stats'}
+									style={{ color: route.route == '/stats' ? '#333' : '' }}
+								>
+									<TrendingUp />
+								</Breadcrumbs.Item>
+							</NextLink>
+							<NextLink href="/notifications">
+								<Breadcrumbs.Item
+									nextLink
+									aria-disabled={route.route == '/notifications'}
+									style={{
+										color: route.route == '/notifications' ? '#333' : '',
+									}}
+								>
+									<Inbox />
+								</Breadcrumbs.Item>
+							</NextLink>
+							<NextLink href="/tuber">
+								<Breadcrumbs.Item
+									nextLink
+									aria-disabled={route.route == '/tuber'}
+									style={{ color: route.route == '/tuber' ? '#333' : '' }}
+								>
+									<Tv />
+								</Breadcrumbs.Item>
+							</NextLink>
+							<NextLink href="/notes">
+								<Breadcrumbs.Item
+									nextLink
+									aria-disabled={route.route == '/notes'}
+									style={{ color: route.route == '/notes' ? '#333' : '' }}
+								>
+									NOTEPAD_ICON
+								</Breadcrumbs.Item>
+							</NextLink>
+						</Breadcrumbs>
+						<Spacer x={0.5} />
+						<Select initialValue={themeType} onChange={darkMode.toggle}>
+							<Select.Option value="light">Light</Select.Option>
+							<Select.Option value="dark">Dark</Select.Option>
+						</Select>
+					</Container>
 					<Component {...pageProps} />
 				</Page>
 			</GeistProvider>
