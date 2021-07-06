47277afa6ed7bb51d8283c8d72089141bcff5767
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-06 10:15:58 +0100

    Add GitHub link to Vext

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, Spacer } from '@geist-ui/react';
+import { Text, Spacer, Container, Link } from '@geist-ui/react';
+import { Github } from '@geist-ui/react-icons';
 
 export default function Projects() {
 	return (
@@ -8,10 +9,16 @@ export default function Projects() {
 			<Text h1 b>
 				Projects
 			</Text>
-			<Text p>All of my current projects</Text>
-			<Text p>
-				<b>vext</b> - A code editor that lives in the terminal.
-			</Text>
+			<Text p>All of my projects</Text>
+			<Container>
+				<Text p>
+					<b>vext</b> - A code editor that lives in the terminal.{' '}
+					<Link href="https://github.com/Milo123459/vext">
+						<Github />
+					</Link>
+				</Text>
+				<Spacer />
+			</Container>
 		</>
 	);
 }
