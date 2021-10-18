f66320d523c192ec5602e4108da9033f1437ba72
Milo <50248166+Milo123459@users.noreply.github.com>
2021-10-18 21:36:11 +0100

    create a transition component to reduce on duplicated code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,19 @@
 import { Text, Grid } from '@geist-ui/react';
 import { motion } from 'framer-motion';
+import Transition from '../components/Transition';
 
 export default function Home() {
 	return (
 		<>
 			<Grid.Container justify="center">
-				<motion.div
-					initial="hidden"
-					animate="visible"
-					variants={{
-						hidden: {
-							scale: 0.8,
-							opacity: 0,
-						},
-						visible: {
-							scale: 1,
-							opacity: 1,
-							transition: {
-								delay: 0.4,
-							},
-						},
-					}}
-				>
+				<Transition delay={0.4}>
 					<Text h1>Hey! &nbsp;I&apos;m Milo 👋</Text>
-				</motion.div>
+				</Transition>
 			</Grid.Container>
 			<Grid.Container justify="center">
-				<motion.div
-					initial="hidden"
-					animate="visible"
-					variants={{
-						hidden: {
-							scale: 0.8,
-							opacity: 0,
-						},
-						visible: {
-							scale: 1,
-							opacity: 1,
-							transition: {
-								delay: 0.8,
-							},
-						},
-					}}
-				>
+				<Transition delay={0.8}>
 					<Text p>I like to program things</Text>
-				</motion.div>
+				</Transition>
 			</Grid.Container>
 		</>
 	);
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,51 +1,20 @@
 import { Text, Grid } from '@geist-ui/react';
 import { motion } from 'framer-motion';
 import OpenSourceProjects from '../components/OpenSourceProjects';
+import Transition from '../components/Transition';
 
 export default function Home() {
 	return (
 		<>
 			<Grid.Container justify="center">
-				<motion.div
-					initial="hidden"
-					animate="visible"
-					variants={{
-						hidden: {
-							scale: 0.8,
-							opacity: 0,
-						},
-						visible: {
-							scale: 1,
-							opacity: 1,
-							transition: {
-								delay: 0.4,
-							},
-						},
-					}}
-				>
+				<Transition delay={0.4}>
 					<Text h1>Projects</Text>
-				</motion.div>
+				</Transition>
 			</Grid.Container>
 			<Grid.Container justify="center">
-				<motion.div
-					initial="hidden"
-					animate="visible"
-					variants={{
-						hidden: {
-							scale: 0.8,
-							opacity: 0,
-						},
-						visible: {
-							scale: 1,
-							opacity: 1,
-							transition: {
-								delay: 0.8,
-							},
-						},
-					}}
-				>
+				<Transition delay={0.8}>
 					<OpenSourceProjects />
-				</motion.div>
+				</Transition>
 			</Grid.Container>
 		</>
 	);
