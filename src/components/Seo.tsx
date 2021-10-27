cce8c5941df884f79a4be687b0f953aff3418b4c
Milo <50248166+Milo123459@users.noreply.github.com>
2021-10-27 17:58:12 +0100

    add a meta description

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,6 +5,7 @@ export default function SEO() {
 			<Head>
 				<meta property="og:title" content="milo.solar" />
 				<meta property="og:description" content="Milo's personal website" />
+				<meta property="description" content="Milo's personal website" />
 				<title>Milo&apos;s website</title>
 			</Head>
 		</>
