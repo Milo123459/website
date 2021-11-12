957c030916093d9c01819fe7b0e4ed5807d78183
Milo <50248166+Milo123459@users.noreply.github.com>
2021-11-12 15:51:57 +0000

    remove bad middleware

--- src/pages/_middleware.tsx
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export default function middleware(req: NextRequest) {
-	console.log(`${req.method} ${req.url}`);
-	return NextResponse.next();
-}
