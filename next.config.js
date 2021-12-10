e5a9f93a4f8768e6c36b64c05d768c8075928483
Milo <50248166+Milo123459@users.noreply.github.com>
2021-12-10 07:26:13 +0000

    bump deps

--- next.config.js
+++ next.config.js
@@ -6,16 +6,4 @@ const { withContentlayer } = require('next-contentlayer');
 module.exports = withContentlayer()({
 	reactStrictMode: true,
 	swcMinify: true,
-	webpack: (config, { dev, isServer }) => {
-		// Replace React with Preact only in client production build
-		if (!dev && !isServer) {
-			Object.assign(config.resolve.alias, {
-				react: 'preact/compat',
-				'react-dom/test-utils': 'preact/test-utils',
-				'react-dom': 'preact/compat',
-			});
-		}
-
-		return config;
-	},
 });
