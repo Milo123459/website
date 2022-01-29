a80e61c74833746036f7536e2715b5827bddadc5
Milo <50248166+Milo123459@users.noreply.github.com>
2022-01-29 22:48:21 +0000

    prevent white flashing if on dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<ThemeProvider attribute="class">
+			<ThemeProvider attribute="class" defaultTheme="system">
 				<Navigation />
 				<Component {...pageProps} />
 			</ThemeProvider>
