f6cdac35f4fef2ff1e6378a9cab8094a5a3c5c8e
Milo <50248166+Milo123459@users.noreply.github.com>
2022-01-29 22:41:05 +0000

    make font loading use swap as display method

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link
-						href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=optional"
+						href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
