ba0a7c00eb295b512e2644bbb09a6f0c9715bbf2
Milo <50248166+Milo123459@users.noreply.github.com>
2024-04-22 12:48:59 +0100

    not even using that font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "milo",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <main className="flex min-h-screen flex-col items-center w-full font-mono text-sm lg:flex lg:flex-col lg:items-center p-8">
           {children}
         </main>
