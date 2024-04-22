c55f7a660a031d974a5e539c8a6f0f7211a0e4fd
Milo <50248166+Milo123459@users.noreply.github.com>
2024-04-22 13:05:05 +0100

    Revert "bye spacre"
    This reverts commit 3605cb0f4ffc8ae08cdac8befd0431ba34771962.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,7 @@ export default function Home() {
           description="i have experience with many other languages and tools, but these are my favorites and deserve to be put on the homepage"
         />
       </Container>
+      <Spacer />
       <Title>projects</Title>
       <Container>
         <Card
